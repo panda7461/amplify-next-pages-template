9419e9ab3fe85f1100cd062ca455e4240bf7ed8e
panda7461 <panda7461@gmail.com>
2025-01-11 17:12:50 +0900

    added authenticator

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,11 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+	externalProviders: {
+
+	}
   },
+  userAttributes: {
+	
+  }
 });
